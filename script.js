706cbe64928d801cc897c242246005f637fdd718
Bharat <hibharatj@gmail.com>
2025-06-24 12:32:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,15 +2,17 @@ const audios = [
   document.getElementById("audio0"),
   document.getElementById("audio1"),
   document.getElementById("audio2"),
-  document.getElementById("audio3"),
-  document.getElementById("audio4"),
-  document.getElementById("audio5"),
 ];
 
 function playSound(index) {
   stopSounds();
   const audio = audios[index];
-  audio.play();
+  if (audio) {
+    audio.play().catch(error => {
+      console.error("Playback failed:", error);
+      alert("Audio playback failed.");
+    });
+  }
 }
 
 function stopSounds() {
